029f2de59e899e3a88217b02f97714e552c24079
Italo Silva <italo.silva.silvera@outlook.com>
2022-06-07 17:22:26 -0500

    chore: base code

---
+++ src/main/java/com/acme/mylawyerbe/MyLawyerBeApplication.java
@@ -0,0 +1,13 @@
+package com.acme.mylawyerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLawyerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLawyerBeApplication.class, args);
+    }
+
+}
