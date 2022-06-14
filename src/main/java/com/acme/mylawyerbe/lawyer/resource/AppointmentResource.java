32fa39747500c87fa8a6046cd931120a6aaae754
AleSilva04 <81649035+AleSilva04@users.noreply.github.com>
2022-06-13 22:54:50 -0500

    feat: added appointment resource

---
+++ src/main/java/com/acme/mylawyerbe/lawyer/resource/AppointmentResource.java
@@ -0,0 +1,11 @@
+package com.acme.mylawyerbe.lawyer.resource;
+
+import java.util.Date;
+
+public class AppointmentResource {
+
+    private Long id;
+    private Date date;
+    private ClientResource client;
+    private LawyerResource lawyer;
+}
