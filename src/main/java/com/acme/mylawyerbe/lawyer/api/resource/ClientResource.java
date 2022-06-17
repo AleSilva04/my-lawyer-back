87c36b420eb3061a3fe8381d768ca8b0205a1ef9
Italo Silva <italo.silva.silvera@outlook.com>
2022-06-17 15:56:10 -0500

    fix: dependencies for client resource.

--- src/main/java/com/acme/mylawyerbe/lawyer/api/resource/ClientResource.java
+++ src/main/java/com/acme/mylawyerbe/lawyer/api/resource/ClientResource.java
@@ -1,21 +1,13 @@
 package com.acme.mylawyerbe.lawyer.api.resource;
 
-import com.acme.mylawyerbe.lawyer.domain.model.entity.Client;
-import com.acme.mylawyerbe.shared.mapping.EnhancedModelMapper;
 import lombok.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
-import java.io.Serializable;
-import java.util.List;
 
 @Getter
 @Setter
 @With
 @NoArgsConstructor
 @AllArgsConstructor
-public class ClientResource implements Serializable {
+public class ClientResource {
 
     private Long id;
 
